7385ca7a2ab23a9a9c179e5549a451421afa4bc8
Tomáš Mráz <tomas.mraz@zona64.cz>
2026-05-04 21:57:58 +0200

    fix time handling on windows

--- src/engine.zig
+++ src/engine.zig
@@ -211,9 +211,24 @@ pub const Engine = struct {
 };
 
 fn nowNs() u64 {
-    var ts: std.os.linux.timespec = undefined;
-    _ = std.os.linux.clock_gettime(.MONOTONIC, &ts);
-    return @as(u64, @intCast(ts.sec)) * std.time.ns_per_s + @as(u64, @intCast(ts.nsec));
+    return switch (@import("builtin").os.tag) {
+        .windows => blk: {
+            var counter: std.os.windows.LARGE_INTEGER = undefined;
+            var freq: std.os.windows.LARGE_INTEGER = undefined;
+            _ = std.os.windows.ntdll.RtlQueryPerformanceCounter(&counter);
+            _ = std.os.windows.ntdll.RtlQueryPerformanceFrequency(&freq);
+            const c: u64 = @intCast(counter);
+            const f: u64 = @intCast(freq);
+            break :blk @divFloor(c, f) * std.time.ns_per_s +
+                @divFloor((c % f) * std.time.ns_per_s, f);
+        },
+        else => blk: {
+            var ts: std.c.timespec = undefined;
+            _ = std.c.clock_gettime(.MONOTONIC, &ts);
+            break :blk @as(u64, @intCast(ts.sec)) * std.time.ns_per_s +
+                @as(u64, @intCast(ts.nsec));
+        },
+    };
 }
 
 fn rayTracingDeviceOptions() ash.DeviceOptions {
